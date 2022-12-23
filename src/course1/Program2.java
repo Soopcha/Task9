627ce5d0c72b1e9896c72276fe85fb1fa75d0233
Soopcha <lena.malykhina.04@gmail.com>
2022-12-23 16:41:56 +0300

    уррр2

---
+++ src/course1/Program2.java
@@ -0,0 +1,24 @@
+package course1;
+
+import java.util.Locale;
+
+import util.SwingUtils;
+
+public class Program2 {
+
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.ROOT);
+        //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        SwingUtils.setDefaultFont("Times New Roman", 18);
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FrameMain().setVisible(true);
+            }
+        });
+
+
+    }
+}
+
